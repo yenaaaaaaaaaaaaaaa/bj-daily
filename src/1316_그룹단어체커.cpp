2cdcda8aac0bea6cad0e93c95c2afff4fa541be0
yenaaaaaaaaaaaaaaa <165683650+yenaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-08-18 23:38:55 +0900

    Create 1316_그룹단어체커.cpp

---
+++ src/1316_그룹단어체커.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int N, cnt = 0; cin >> N;
+	char t;
+	
+	
+	for (int i = 0; i < N;i++) {
+		bool g = true;
+		vector<int> arr(26, 0);
+		string s; cin >> s;
+		t = s[0];
+		arr[t - 'a'] += 1;
+
+		for (int j = 1; j < s.size();) {
+			if (s[j] != t) {
+				if ((arr[s[j] - 'a'] == 0)) {
+					t = s[j];
+					arr[t - 'a'] += 1;
+					j++;
+				}
+				else { 
+					j = s.size(); 
+					g = false;
+				}
+			}
+			else {
+				t = s[j];
+				arr[t - 'a'] += 1;
+				j++;
+			}
+		}
+		if (g == true) { cnt++; }
+	}
+	cout << cnt;
+}
+
