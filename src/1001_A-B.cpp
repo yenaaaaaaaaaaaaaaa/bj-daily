9b8d0ceef6725a98df088e6cccf58fb61669c3a3
yenaaaaaaaaaaaaaaa <165683650+yenaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-09-16 15:23:50 +0900

    Create 1001_A-B.cpp

---
+++ src/1001_A-B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+#include <cctype>
+#include <map>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int A, B;
+    cin >> A >> B;
+    cout<<  A - B;
+    return 0;
+   }
