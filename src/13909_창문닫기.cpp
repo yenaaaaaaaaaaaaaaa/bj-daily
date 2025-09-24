040b4b549ed35e572048e3d8d9419a5aa24e3f42
yenaaaaaaaaaaaaaaa <165683650+yenaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-09-24 14:32:05 +0900

    Create 13909_창문닫기.cpp

---
+++ src/13909_창문닫기.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+#include <cctype>
+#include <map>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int N;
+	cin >> N;
+	int a = sqrt(N);
+	cout << a;
+}
+
