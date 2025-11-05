33da19007de498b9b4a2712a18ec8818f671f932
yenaaaaaaaaaaaaaaa <165683650+yenaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-11-05 20:37:51 +0900

    Create 24723_녹색거탑.cpp

---
+++ src/24723_녹색거탑.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int N; cin >> N;
+	cout << pow(2,N);
+	return 0;
+}
+
