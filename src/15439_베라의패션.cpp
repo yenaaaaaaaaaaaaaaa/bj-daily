8c1bdba2f4285b9325de6fecb5ff7a56f3511966
yenaaaaaaaaaaaaaaa <165683650+yenaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-11-05 20:33:16 +0900

    Create 15439_베라의패션.cpp

---
+++ src/15439_베라의패션.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int N; cin >> N;
+	cout << N * (N - 1);
+	return 0;
+}
+
